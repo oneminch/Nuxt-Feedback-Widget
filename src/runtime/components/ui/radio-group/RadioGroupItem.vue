f8d660665ebe9358ce9b5292dd0d08792f34ee58
oneminch <27751688+oneminch@users.noreply.github.com>
2025-06-03 12:44:22 -0400

    chore: misc
    - add more shadcn-vue components
    - add svg icons for feedback options
    - clean up configurations

---
+++ src/runtime/components/ui/radio-group/RadioGroupItem.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { RadioGroupItemProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+import { reactiveOmit } from "@vueuse/core";
+import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from "reka-ui";
+import { cn } from "../../../lib/utils";
+
+const props = defineProps<
+  RadioGroupItemProps & { class?: HTMLAttributes["class"] }
+>();
+
+const delegatedProps = reactiveOmit(props, "class");
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <RadioGroupItem
+    data-slot="radio-group-item"
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'relative border-input text-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-12 flex items-center justify-center shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer',
+        props.class,
+      )
+    "
+  >
+    <slot />
+    <RadioGroupIndicator
+      data-slot="radio-group-indicator"
+      class="absolute inset-0 -z-10 rounded-full border border-lime-500 bg-lime-100 flex items-center justify-center"
+    />
+  </RadioGroupItem>
+</template>
