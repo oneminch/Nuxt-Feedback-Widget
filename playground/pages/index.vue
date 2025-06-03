4fc9c70ee9ab1c980c40cb308b98c3b826964483
oneminch <27751688+oneminch@users.noreply.github.com>
2025-06-03 14:14:38 -0400

    refactor: make playground page responsive

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="absolute inset-0 flex items-center justify-center">
     <div
-      class="w-2/3 h-4/5 grid grid-cols-5 gap-8 *:border-[0.5px] *:border-neutral-200 *:bg-neutral-50 *:p-10 *:rounded-2xl"
+      class="w-5/6 h-5/6 grid grid-rows-2 md:grid-rows-1 md:grid-cols-5 gap-8 *:border-[0.5px] *:border-neutral-200 *:bg-neutral-50 *:p-10 *:rounded-2xl"
     >
-      <div class="flex flex-col items-center justify-center gap-8 col-span-3">
+      <div
+        class="flex flex-col items-center justify-center gap-8 md:col-span-3"
+      >
         <h2
           class="text-3xl max-w-4/5 text-neutral-700 font-semibold text-center"
         >
@@ -16,7 +18,7 @@
           trigger-class="bg-white hover:bg-neutral-50 text-neutral-800 border border-neutral-300 shadow-none"
         />
       </div>
-      <div class="flex items-center justify-center relative col-span-2">
+      <div class="flex items-center justify-center relative md:col-span-2">
         <h2
           class="text-xl max-w-4/5 text-neutral-700 font-semibold text-center"
         >
